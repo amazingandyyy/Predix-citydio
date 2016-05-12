280f1b9b74dd6b18890ad8be2b28bd51fcf85305
amazingandyyy <amazingandyyy@gmail.com>
2016-05-12 15:08:43 -0700

    before pres
    before pres

--- app.js
+++ app.js
@@ -129,5 +129,31 @@ app.controller('mainCtrl', function($scope, $http, $timeout) {
             })
     }
 
+    $(function() {
+        var params = {
+            // Request parameters
+            "visualFeatures": "Categories",
+            "details": "{string}",
+        };
+
+        $.ajax({
+            url: "https://api.projectoxford.ai/vision/v1.0/analyze?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","{subscription key}");
+            },
+            type: "POST",
+            // Request body
+            data: "{body}",
+        })
+        .done(function(data) {
+            alert("success");
+        })
+        .fail(function() {
+            // alert("error");
+        });
+    });
+
 
 });
